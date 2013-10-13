86d898c4bf8494796d31bc8efba485e39350cbdb
Nidhi Kulkarni <nkulkarni@okta.com>
2013-10-12 21:53:19 -0700

    Initial Commit

---
+++ Todo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Todo
+//
+//  Created by Nidhi Kulkarni on 10/12/13.
+//  Copyright (c) 2013 Nidhi Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
